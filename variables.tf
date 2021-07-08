e9ea8544cc09783b6e971803c269f42e19fc8f0d
Mohius Sakian <sakian2@gmail.com>
2021-07-08 00:10:44 -0400

    added terraform files to feature branch

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = "string"
+    default = "AKIAYC75MKQGYZWEUC6Q"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "yBNAnzIMq1dTXXuGJX0u0PuRRhMn8P7LGMXiqb6g"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = "string"
+    default = ""
+}
